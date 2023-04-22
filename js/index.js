b4209a803e0dc990fcec92aebba3c2eda1e5d7ed
bhavesh <bhavesh@ebco.in>
2023-04-22 10:14:20 +0530

    @media touch for carousel2

--- js/index.js
+++ js/index.js
@@ -6,6 +6,23 @@ for(i=0; i<quadimages.length; i++) {
   quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") }); 
 }
 
+/* For swipe feature  */
+
 /* $('.carousel').bcSwipe({ threshold: 50 }); */
+
+/* $(document).ready(function() {
+   $("#myCarousel").swiperight(function() {
+      $(this).carousel('prev');
+    });
+   $("#myCarousel").swipeleft(function() {
+      $(this).carousel('next');
+   });
+}); */
+
+/* $(document).on("pagecreate","#myCarousel",function(){
+  $("p").on("swipe",function(){
+    $(this).hide();
+  });                       
+}); */
 
 console.log("Exit JS")
